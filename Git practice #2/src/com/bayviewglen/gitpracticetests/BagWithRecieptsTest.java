17834f3c151462e61c59c6147da6623f9e525d4b
mhusseinbvg <mhussein@bayviewglen.ca>
2014-09-16 20:32:00 -0400

    This is the tests gile

---
+++ Git practice #2/src/com/bayviewglen/gitpracticetests/BagWithRecieptsTest.java
@@ -0,0 +1,61 @@
+package com.bayviewglen.gitpracticetests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import com.bayviewglen.gitpractice.BagWithReciepts;
+
+public class BagWithRecieptsTest extends BagWithReciepts {
+
+	@Test
+	public void testBagWithReciepts() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAdd() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testClone() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCountOccurrences() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testReceipts() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testRemove() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testRemove_by_receipt() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testRetrieve() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSize() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testUsing_receipt() {
+		fail("Not yet implemented");
+	}
+
+}
