8d94727449bd6d3317fd325a843e1686e1279f49
mhusseinbvg <mhussein@bayviewglen.ca>
2014-09-16 20:43:46 -0400

    The other two test files, because apparently we have to have them

---
+++ Git practice #2/src/com/bayviewglen/gitpracticetests/ListerTest.java
@@ -0,0 +1,37 @@
+package com.bayviewglen.gitpracticetests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import com.bayviewglen.gitpractice.Lister;
+import com.bayviewglen.gitpractice.Node;
+
+public class ListerTest extends Lister {
+
+	public ListerTest(Node head) {
+		super(head);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Test
+	public void testLister() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testHasNext() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testNext() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testRemove() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ Git practice #2/src/com/bayviewglen/gitpracticetests/NodeTest.java
@@ -0,0 +1,81 @@
+package com.bayviewglen.gitpracticetests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import com.bayviewglen.gitpractice.Node;
+
+public class NodeTest extends Node {
+
+	public NodeTest(Object initialData, Node initialLink) {
+		super(initialData, initialLink);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Test
+	public void testNode() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAddNodeAfter() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetData() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetLink() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testListCopy() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testListCopyWithTail() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testListLength() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testListPart() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testListPosition() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testListSearch() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testRemoveNodeAfter() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetData() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetLink() {
+		fail("Not yet implemented");
+	}
+
+}
